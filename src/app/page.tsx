8273012f7bb575cd365faab495007a233b49af43
John Ritterbush <jritterbush@gmail.com>
2023-08-26 12:39:55 -0500

    install nextjs

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main id="main-content" className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Fire Arts Center</h1>
+    </main>
+  )
+};
