d4c13a9ce1dfee6671a6d86b07dbb9087c4a6abf
John Ritterbush <jritterbush@gmail.com>
2023-08-26 12:49:25 -0500

    change to pages router

--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main id="main-content" className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Fire Arts Center</h1>
-    </main>
-  )
-};
