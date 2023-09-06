2be9463bef7f4a0323d68ad6976debb7e2d98148
John Ritterbush <jritterbush@gmail.com>
2023-09-06 16:36:22 -0500

    simplify contact page

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Button from '../components/Button';
 import {org} from '../constants';
 
 export default function ContactPage() {
@@ -9,23 +10,14 @@ export default function ContactPage() {
 				forging, stone carving, figure sculpture, figure drawing or just want to check out the Fire Arts Center of
 				Chicago’s studio, please contact us and let us know.
 			</p>
-			<p className='mt-4'>You can reach us by email at either of the email addresses below:</p>
-			<p className='ml-5 my-4'>
-				<Link href={`mailto:${org.email}`}>Fire Arts Info</Link>
-			</p>
-			<p className='mt-4'>You can contact us by phone at any of the following phone numbers:</p>
-			<p className='ml-5 my-4'>{org.phone}</p>
-
-			<p className='mt-4'>
-				And finally you can send a letter or stop by the studio which is located at the following address:
-			</p>
-			<p className='ml-5 my-4'>
-				{org.name}
-				<br />
-				{org.address}
-				<br />
-				{org.city}, {org.state} {org.zip}
-			</p>
+			<div className='flex justify-around mt-6'>
+				<Button styleType='primary' href={`mailto:${org.email}`}>
+					Email the Fire Arts Center
+				</Button>
+				<Button href={`tel:+1${org.phone.split('.').join('')}`} styleType='secondary'>
+					Call {org.phone}
+				</Button>
+			</div>
 		</section>
 	);
 }
